b797c5e9821076d22f66360847be7e2f3a21d489
Arjun Kumar <kumararjundamai1999@gmail.com>
2024-04-21 05:25:45 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Bar from "./scenes/bar";
@@ -20,7 +19,6 @@ function App() {
         <div className="app">
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/bar" element={<Bar />} />
