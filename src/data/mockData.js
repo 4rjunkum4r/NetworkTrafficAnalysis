8a587ae63c291dd3796b25f39ccd0203ef836cf8
Arjun Kumar <kumararjundamai1999@gmail.com>
2024-04-21 05:05:30 +0530

    project

---
+++ src/data/mockData.js
@@ -0,0 +1,354 @@
+import { tokens } from "../theme";
+
+export const mockTraffic = [
+  {
+    txId: "01e4dsa",
+    user: "11916159",
+    date: "2021-09-01 10:24 PM",
+    usage: "43.95",
+  },
+  {
+    txId: "0315dsaa",
+    user: "11907201",
+    date: "2021-09-01 10:00 PM",
+    usage: "133.45",
+  },
+  {
+    txId: "01e4dsa",
+    user: "12207242",
+    date: "2021-09-01 9:24 PM",
+    usage: "43.95",
+  },
+  {
+    txId: "51034szv",
+    user: "12202356",
+    date: "2022-11-05",
+    usage: "200.95",
+  },
+  {
+    txId: "0a123sb",
+    user: "11912345",
+    date: "2022-11-02",
+    usage: "13.55",
+  },
+  {
+    txId: "01e4dsa",
+    user: "12210121",
+    date: "2021-09-01",
+    usage: "43.95",
+  },
+  {
+    txId: "120s51a",
+    user: "12212122",
+    date: "2019-04-15",
+    usage: "24.20",
+  },
+  {
+    txId: "0315dsaa",
+    user: "12222211",
+    date: "2021-09-01 10:24 PM",
+    usage: "133.45",
+  },
+  {
+    txId: "0315dsaa",
+    user: "12301202",
+    date: "2021-09-01 10:24 PM",
+    usage: "133.45",
+  },
+  {
+    txId: "0315dsaa",
+    user: "12345678",
+    date: "2021-09-01 10:24 PM",
+    usage: "133.45",
+  },
+  {
+    txId: "0315dsaa",
+    user: "11287653",
+    date: "2021-09-01 10:24 PM",
+    usage: "133.45",
+  },
+  {
+    txId: "0315dsaa",
+    user: "12239384",
+    date: "2021-09-01 10:24 PM",
+    usage: "133.45",
+  },
+];
+
+export const mockBarData = [
+  {
+    registrationNumber: "AD",
+    "hot dog": 137,
+    "hot dogColor": "hsl(229, 70%, 50%)",
+    burger: 96,
+    burgerColor: "hsl(296, 70%, 50%)",
+    kebab: 72,
+    kebabColor: "hsl(97, 70%, 50%)",
+    donut: 140,
+    donutColor: "hsl(340, 70%, 50%)",
+  },
+  {
+    registrationNumber: "AE",
+    "hot dog": 55,
+    "hot dogColor": "hsl(307, 70%, 50%)",
+    burger: 28,
+    burgerColor: "hsl(111, 70%, 50%)",
+    kebab: 58,
+    kebabColor: "hsl(273, 70%, 50%)",
+    donut: 29,
+    donutColor: "hsl(275, 70%, 50%)",
+  },
+  {
+    registrationNumber: "AF",
+    "hot dog": 109,
+    "hot dogColor": "hsl(72, 70%, 50%)",
+    burger: 23,
+    burgerColor: "hsl(96, 70%, 50%)",
+    kebab: 34,
+    kebabColor: "hsl(106, 70%, 50%)",
+    donut: 152,
+    donutColor: "hsl(256, 70%, 50%)",
+  },
+  {
+    registrationNumber: "AG",
+    "hot dog": 133,
+    "hot dogColor": "hsl(257, 70%, 50%)",
+    burger: 52,
+    burgerColor: "hsl(326, 70%, 50%)",
+    kebab: 43,
+    kebabColor: "hsl(110, 70%, 50%)",
+    donut: 83,
+    donutColor: "hsl(9, 70%, 50%)",
+  },
+  {
+    registrationNumber: "AI",
+    "hot dog": 81,
+    "hot dogColor": "hsl(190, 70%, 50%)",
+    burger: 80,
+    burgerColor: "hsl(325, 70%, 50%)",
+    kebab: 112,
+    kebabColor: "hsl(54, 70%, 50%)",
+    donut: 35,
+    donutColor: "hsl(285, 70%, 50%)",
+  },
+  {
+    registrationNumber: "AL",
+    "hot dog": 66,
+    "hot dogColor": "hsl(208, 70%, 50%)",
+    burger: 111,
+    burgerColor: "hsl(334, 70%, 50%)",
+    kebab: 167,
+    kebabColor: "hsl(182, 70%, 50%)",
+    donut: 18,
+    donutColor: "hsl(76, 70%, 50%)",
+  },
+  {
+    registrationNumber: "AM",
+    "hot dog": 80,
+    "hot dogColor": "hsl(87, 70%, 50%)",
+    burger: 47,
+    burgerColor: "hsl(141, 70%, 50%)",
+    kebab: 158,
+    kebabColor: "hsl(224, 70%, 50%)",
+    donut: 49,
+    donutColor: "hsl(274, 70%, 50%)",
+  },
+];
+
+export const mockPieData = [
+  {
+    id: "Over 1000 MB",
+    label: "Over 1000 MB",
+    value: 239,
+    color: "hsl(104, 70%, 50%)",
+  },
+  {
+    id: "750-1000 MB",
+    label: "750-1000 MB",
+    value: 170,
+    color: "hsl(162, 70%, 50%)",
+  },
+  {
+    id: "500-750 MB",
+    label: "500-750 MB",
+    value: 322,
+    color: "hsl(291, 70%, 50%)",
+  },
+  {
+    id: "250-500 MB",
+    label: "250-500 MB",
+    value: 503,
+    color: "hsl(229, 70%, 50%)",
+  },
+  {
+    id: "0-250 MB",
+    label: "0-250 MB",
+    value: 584,
+    color: "hsl(344, 70%, 50%)",
+  },
+];
+
+export const mockLineData = [
+  {
+    id: "Morning Slot",
+    color: tokens("dark").greenAccent[500],
+    data: [
+      {
+        x: "11916159",
+        y: 101,
+      },
+      {
+        x: "11907201",
+        y: 75,
+      },
+      {
+        x: "12207242",
+        y: 36,
+      },
+      {
+        x: "12202356",
+        y: 216,
+      },
+      {
+        x: "11912345",
+        y: 35,
+      },
+      {
+        x: "12210121",
+        y: 236,
+      },
+      {
+        x: "12212122",
+        y: 88,
+      },
+      {
+        x: "12222211",
+        y: 232,
+      },
+      {
+        x: "12301202",
+        y: 281,
+      },
+      {
+        x: "12345678",
+        y: 1,
+      },
+      {
+        x: "11287653",
+        y: 35,
+      },
+      {
+        x: "12239384",
+        y: 14,
+      },
+    ],
+  },
+  {
+    id: "Afternoon Slot",
+    color: tokens("dark").blueAccent[300],
+    data: [
+      {
+        x: "11916159",
+        y: 212,
+      },
+      {
+        x: "11907201",
+        y: 190,
+      },
+      {
+        x: "12207242",
+        y: 270,
+      },
+      {
+        x: "12202356",
+        y: 9,
+      },
+      {
+        x: "11912345",
+        y: 75,
+      },
+      {
+        x: "12210121",
+        y: 175,
+      },
+      {
+        x: "12212122",
+        y: 33,
+      },
+      {
+        x: "12222211",
+        y: 189,
+      },
+      {
+        x: "12301202",
+        y: 97,
+      },
+      {
+        x: "12345678",
+        y: 87,
+      },
+      {
+        x: "11287653",
+        y: 299,
+      },
+      {
+        x: "12239384",
+        y: 251,
+      },
+    ],
+  },
+  {
+    id: "Night Slot",
+    color: tokens("dark").redAccent[200],
+    data: [
+      {
+        x: "11916159",
+        y: 191,
+      },
+      {
+        x: "11907201",
+        y: 136,
+      },
+      {
+        x: "12207242",
+        y: 91,
+      },
+      {
+        x: "12202356",
+        y: 190,
+      },
+      {
+        x: "11912345",
+        y: 211,
+      },
+      {
+        x: "12210121",
+        y: 152,
+      },
+      {
+        x: "12212122",
+        y: 189,
+      },
+      {
+        x: "12222211",
+        y: 152,
+      },
+      {
+        x: "12301202",
+        y: 8,
+      },
+      {
+        x: "12345678",
+        y: 197,
+      },
+      {
+        x: "11287653",
+        y: 107,
+      },
+      {
+        x: "12239384",
+        y: 170,
+      },
+    ],
+  },
+];
